d0259c3409ebdbf82938c18bda8fd3b69cefe20a
Alex <alexandre.vernet99@gmail.com>
2022-11-03 15:09:44 +0100

    Feat - Install mongoose

--- index.ts
+++ index.ts
@@ -1,13 +1,28 @@
 import { Request, Response } from 'express';
 import express = require('express');
 
+const mongoose = require('mongoose');
 const app = express();
 
 app.set('view engine', 'pug');
 
+// Connect to database
+mongoose.connect('mongodb+srv://arnal:azerty31@cluster0.0ixho.mongodb.net/?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        // useCreateIndex: true,
+        // useFindAndModify: false,
+    }
+);
+
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello world!');
+    // Get data from database cookbook and collection 'recipes'
+    mongoose.connection.db.collection('recipes', (err: any, collection: any) => {
+        collection.find().toArray((err: any, result: any) => {
+            if (err) throw err;
+            res.send(result);
+        });
+    })
 });
 
 app.listen(3000, () => {
