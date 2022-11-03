5f4c64a91b37c8a033369f06162a1d5aeb321392
thomaslebn <thomas.lebn@protonmail.com>
2022-11-03 16:41:03 +0100

    FEAT - Add form for creating recipe

--- index.ts
+++ index.ts
@@ -1,14 +1,34 @@
-import { Request, Response } from 'express';
+import {Request, Response} from 'express';
 import express = require('express');
 
 const app = express();
 
 app.set('view engine', 'pug');
 
-
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello world!');
 });
+
+app.get('/form', (req, res) => {
+    res.render('form')
+})
+
+app.post('/form', (req, res) => {
+    /*    const recipeData = new recipeModel({
+            name: req.body.name,
+            categories: req.body.categories,
+            time: req.body.time,
+            difficulty: req.body.difficulty,
+            description: req.body.description,
+            ingredients: req.body.ingredients
+        })
+        recipeData.save().then(data => {
+            res.render('/');
+        }).catch(err => {
+            res.render('/form', );
+        });
+        res.render('/form')*/
+})
 
 app.listen(3000, () => {
     console.log('🚀 Server is running on port 3000');
