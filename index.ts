8df1db76109e51c78e2572493bbecfbe76e68e83
Brieuc Fresnel <brieuc@studio-dot.fr>
2022-11-03 17:15:19 +0100

    ça marche à peu près

--- index.ts
+++ index.ts
@@ -1,43 +1,26 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
 
-const express = require('express');
-const mongoose = require('mongoose');
-const Recipes = require('./schemas/Recipes');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const Recipes = require("./schemas/Recipes");
+require("dotenv").config();
 const app = express();
 
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 
-app.get('/', (req: Request, res: Response) => {
-
-app.get('/form', (req, res) => {
-    res.render('form')
-})
-
-app.post('/form', (req, res) => {
-   const recipeData = new recipeModel({
-      name: req.body.name,
-      categories: req.body.categories,
-      time: req.body.time,
-      difficulty: req.body.difficulty,
-      description: req.body.description,
-      ingredients: req.body.ingredients
-    })
-    recipeData.save().then(data => {
-        res.render('/');
-    }).catch(err => {
-        res.render('/form', );
+mongoose
+  .connect(process.env.MONGO_DB_URI, { dbName: "cookbook" })
+  .then(async () => {
+    console.log("Connected to database");
+    app.listen(3000, () => {
+      console.info("Server started on port 3000");
     });
-    res.render('/form')*/
-})
+  })
+  .catch(console.error);
 
-app.listen(3000, () => {
-    console.log('🚀 Server is running on port 3000');
-    // Get data from table recipes and pass it to the view
-    
-    Recipes.find({})
-        .then((result: any) => {
-                res.render('index', { recipes: result });
-            }
-        );
+app.get("/", async (req: Request, res: Response) => {
+  // Get data from table recipes and pass it to the view
+  const recipes = await Recipes.find({});
+  console.log(recipes);
+  res.render("index", { recipes: recipes });
 });
--- schemas/Like.ts
+++ schemas/Like.ts
@@ -1,11 +1,11 @@
 // const mongoose = require('mongoose');
 
 const LikeSchema = new mongoose.Schema({
-    // name: String,
-    // difficulty: Number,
-    // description: String,
+  // name: String,
+  // difficulty: Number,
+  // description: String,
 });
 
-const Likes = mongoose.model('likes', LikeSchema);
+const Likes = mongoose.model("likes", LikeSchema);
 
 module.exports = Recipes;
