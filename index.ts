0b7e7dd4e64b08de601503228a84613a4da84ac1
Alex <alexandre.vernet99@gmail.com>
2022-11-03 14:42:00 +0100

    Feat - Add TS

---
+++ index.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import express = require('express');
+
+const app = express();
+
+app.set('view engine', 'pug');
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello world!');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
