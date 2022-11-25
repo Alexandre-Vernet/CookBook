b6d088ca04a94f50b6fbb0561248679ab2eadf10
Alex <alexandre.vernet99@gmail.com>
2022-11-25 11:45:11 +0100

    feat (login): Create view and css

--- index.ts
+++ index.ts
@@ -1,25 +1,31 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
-import * as mongoose from "mongoose";
-import Recipes from "./schemas/Recipes";
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import * as mongoose from 'mongoose';
+import Recipes from './schemas/Recipes';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
-app.set("view engine", "pug");
+app.set('view engine', 'pug');
 
 mongoose
-  .connect(`${process.env.MONGO_DB_URI}`, { dbName: "cookbook" })
-  .then(async () => {
-    app.listen(port, () => {
-      console.info(`Server started on port ${port}`);
-    });
-  })
-  .catch(console.error);
+    .connect(`${ process.env.MONGO_DB_URI }`, { dbName: 'cookbook' })
+    .then(async () => {
+        app.listen(port, () => {
+            console.info(`Server started on port ${ port }`);
+        });
+    })
+    .catch(console.error);
 
-app.get("/", async (req: Request, res: Response) => {
-  // Get data from table recipes and pass it to the view
-  const recipes = await Recipes.find({});
-  res.render("index", { recipes: recipes });
+app.get('/', async (req: Request, res: Response) => {
+    // Get data from table recipes and pass it to the view
+    const recipes = await Recipes.find({});
+    res.render('index', { recipes: recipes });
+});
+
+app.get('/login', (req: Request, res: Response) => {
+    res.render('login', {
+        title: 'Login',
+    });
 });
